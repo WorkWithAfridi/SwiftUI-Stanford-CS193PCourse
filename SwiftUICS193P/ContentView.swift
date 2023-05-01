f792188964d23ada543394ffa7901e3009cbf64b
KhondakarAfridi <afridi.khondakar@gmail.com>
2023-05-01 08:01:49 +0600

    Created init project'

--- SwiftUICS193P/ContentView.swift
+++ SwiftUICS193P/ContentView.swift
@@ -9,18 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }.padding(.horizontal)
+    }
+}
+
+struct CardView : View{
+    var body: some View{
+            ZStack(
+                alignment: .center,
+                content: {
+                    RoundedRectangle(
+                        cornerRadius: 25.0
+                    )
+                    .stroke(
+                        lineWidth: 2
+                    )
+                    .foregroundColor(
+                        Color(
+                            hue: 0.55,
+                            saturation: 0.924,
+                            brightness: 0.798
+                        )
+                    )
+                    RoundedRectangle(
+                        cornerRadius: 25.0
+                    )
+                    .fill()
+                    .foregroundColor(
+                        .white
+                    )
+                    Text("✈️")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.pink)
+                        .multilineTextAlignment(.center)
+                }
+            )
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
