6f2c0e94031dd8791b6134c3a900d3e8b2cb662a
KhondakarAfridi <afridi.khondakar@gmail.com>
2023-05-01 07:51:51 +0600

    Initial Commit

---
+++ SwiftUICS193P/SwiftUICS193PApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICS193PApp.swift
+//  SwiftUICS193P
+//
+//  Created by Khondakar Afridi on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICS193PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
